933468ecce9ddc1efc48d21435b09fc824a32571
Ayanfeoluwa Abiodun <ayanfeoluwabiodun@gmail.com>
2023-08-11 12:42:25 -0700

    My first commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - print all possible combinations of single digit numbers
+ * main - Entry point
  *
- * Return - Always 0 (Success)
+ * Description: Prints all possible combinations of single-digit numbers.
+ * Numbers must be separated by a comma and a space.
+ * Numbers should be printed in ascending order.
+ * You can only use the putchar function.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int num;
+    int num;
 
-	for (num = 0; num <= 9; num++)
-	{
-		putchar('0' + num);
+    for (num = 0; num <= 9; num++)
+    {
+        putchar('0' + num);
 
-		if (num < 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+        if (num < 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
 
-	putchar('\n');
-	return (0);
+    putchar('\n');
+    return (0);
+}
 }

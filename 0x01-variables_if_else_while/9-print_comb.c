ee6995119cedcae802bef9c75a7701938398ec67
Ayanfeoluwa Abiodun <ayanfeoluwabiodun@gmail.com>
2023-08-11 12:49:39 -0700

    My first commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,18 @@
  */
 int main(void)
 {
-    int num;
+	int num = 0;
 
-    for (num = 0; num <= 9; num++)
-    {
-        putchar('0' + num);
-
-        if (num < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-
-    putchar('\n');
-    return (0);
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
 }

513d20b572d030162a62c54e46cda04f0e106bf9
Ayanfeoluwa Abiodun <ayanfeoluwabiodun@gmail.com>
2023-08-15 14:18:24 -0700

    My first commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,46 +2,44 @@
 
 /**
  * print_times_table - prints the n times table
- * @n: The number for which to print the times table
+ * @n: the times table to be printed
  */
 void print_times_table(int n)
 {
+    int i, j;
+
     if (n < 0 || n > 15)
         return;
-
-    int i, j, result;
 
     for (i = 0; i <= n; i++)
     {
         for (j = 0; j <= n; j++)
         {
-            result = i * j;
+            int result = i * j;
+
             if (j == 0)
-            {
-                _putchar('0' + result);
-            }
+                _putchar('0');
             else
             {
                 _putchar(',');
                 _putchar(' ');
+
                 if (result < 10)
-                {
                     _putchar(' ');
+                if (result < 100)
                     _putchar(' ');
-                    _putchar('0' + result);
-                }
-                else if (result >= 10 && result < 100)
+
+                if (result >= 100)
                 {
-                    _putchar(' ');
-                    _putchar('0' + result / 10);
-                    _putchar('0' + result % 10);
+                    _putchar(result / 100 + '0');
+                    _putchar((result % 100) / 10 + '0');
                 }
-                else
+                else if (result >= 10)
                 {
-                    _putchar('0' + result / 100);
-                    _putchar('0' + (result / 10) % 10);
-                    _putchar('0' + result % 10);
+                    _putchar(result / 10 + '0');
                 }
+
+                _putchar(result % 10 + '0');
             }
         }
         _putchar('\n');

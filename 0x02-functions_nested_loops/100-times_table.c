5c9249d63eaefed6965f96086575ad1dd3577512
Ayanfeoluwa Abiodun <ayanfeoluwabiodun@gmail.com>
2023-08-15 12:52:11 -0700

    My first commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,43 +6,43 @@
  */
 void print_times_table(int n)
 {
-    int i, j;
+	int i, j;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+	return;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            int result = i * j;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			int result = i * j;
 
-            if (j == 0)
-                _putchar('0');
-            else
-            {
-                _putchar(',');
-                _putchar('	');
+			if (j == 0)
+			_putchar('0');
+			else
+			{
+				putchar(',');
+				putchar(' ');
 
-                if (result < 10)
-                    _putchar(' ');
-                if (result < 100)
-                    _putchar(' ');
+				if (result < 10)
+				_putchar(' ');
+				if (result < 100)
+				_putchar(' ');
 
-                if (result >= 100)
-                {
-                    _putchar(result / 100 + '0');
-                    _putchar((result % 100) / 10 + '0');
-                }
-                else if (result >= 10)
-                {
-                    _putchar(result / 10 + '0');
-                }
+				if (result >= 100)
+				{
+					_putchar(result / 100 + '0');
+					_putchar((result % 100) / 10 + '0');
+				}
+				else if (result >= 10)
+				{
+					_putchar(result / 10 + '0');
+				}
 
-                _putchar(result % 10 + '0');
-            }
-        }
-        _putchar('\n');
-    }
+				_putchar(result % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
 

f3a1efb05f23cb13bbc2b2bc073ede119f0c61ee
Ayanfeoluwa Abiodun <ayanfeoluwabiodun@gmail.com>
2023-08-17 09:20:36 -0700

    My commit for C-Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* Main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 

6cf484937aafde024adac7390af2a1d2c0e8f03b
Ayanfeoluwa Abiodun <ayanfeoluwabiodun@gmail.com>
2023-08-21 13:45:18 -0700

    My commit for arrays strings and pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,10 +12,10 @@ void print_array(int *a, int n)
 
 	if (a != NULL && n >= 0)
 	{
-		for (i >= 0; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
 			printf("%d", a[i]);
-			if (i < n - 1)
+			if (i <= n - 1)
 				printf(", ");
 		}
 		printf("\n");

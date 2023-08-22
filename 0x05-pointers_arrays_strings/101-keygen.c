8ac246663140f8789cf6009d2e4146acaa7d6a44
Ayanfeoluwa Abiodun <ayanfeoluwabiodun@gmail.com>
2023-08-21 18:24:38 -0700

    My commit for arrays strings and pointers

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,36 +3,21 @@
 #include <time.h>
 
 /**
- * main - random password generation for 101-crackme
- *
- * Return: Always 0.
+ * main - generates random passwords for 101-crackme
+ * Return: zero
  */
 int main(void)
 {
-	int i, j, k, s;
-	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char p[58];
+	int sum;
+	char c;
 
 	srand(time(NULL));
-	while (s != 2772)
+	while (sum <= 2645)
 	{
-		j = rand() % 62;
-		p[i] = c[j];
-		s += c[j];
-		i++;
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
 	}
-	while (c[k])
-	{
-		if (c[k] == (2772 - s))
-		{
-			p[i] = c[k];
-			s += c[k];
-			i++;
-			break;
-		}
-		k++;
-	}
-	p[i] = '\0';
-	printf("%s", p);
+	putchar(2772 - sum);
 	return (0);
 }

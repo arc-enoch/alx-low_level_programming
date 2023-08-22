f412c96417913423ce9945b3b4603459abdf9d4f
Ayanfeoluwa Abiodun <ayanfeoluwabiodun@gmail.com>
2023-08-21 18:04:27 -0700

    My commit for arrays strings and pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,34 +9,34 @@
  */
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int i = 0;
+	int result = 0;
+	int sign = 1;
+	int i = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-        {
-            sign *= -1;
-        }
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-             if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
-            {
-                if (sign == 1)
-                    return INT_MAX;
-                else
-                    return INT_MIN;
-            }
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
 
-            result = result * 10 + (s[i] - '0');
-        }
-        else if (result != 0)
-        {
-            break;
-        }
-        i++;
-    }
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (result != 0)
+		{
+			break;
+		}
+		i++;
+	}
 
-    return result * sign;
+	return (result * sign);
 }

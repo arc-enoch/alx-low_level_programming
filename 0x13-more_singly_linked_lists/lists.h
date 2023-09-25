56d083a9ccce1c3d4f60367c713abfeccfcebb25
Ayanfeoluwa Abiodun <ayanfeoluwabiodun@gmail.com>
2023-09-25 12:41:58 -0700

    My commit for more singly linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -11,6 +11,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ *
  */
 typedef struct listint_s
 {
@@ -34,4 +35,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif /* LIS
+#endif /* LISTS_H */

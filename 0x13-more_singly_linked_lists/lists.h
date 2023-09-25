6b5fa7cee8c502cb19c55ab072210eb0bfda2dc0
Ayanfeoluwa Abiodun <ayanfeoluwabiodun@gmail.com>
2023-09-25 12:37:27 -0700

    My commit for more singly linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define LISTS_H
 
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - singly linked list
@@ -9,7 +11,6 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct listint_s
 {
